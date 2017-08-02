3d2e15d3e7e0fb4bdfb8d17dc77afab0974233c0
Amal <Amal@mohidins-macbook.local>
2017-08-02 09:43:04 -0400

    Initial Commit

---
+++ naturELLE/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  naturELLE
+//
+//  Created by Amal  on 8/2/17.
+//  Copyright © 2017 Amal . All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
